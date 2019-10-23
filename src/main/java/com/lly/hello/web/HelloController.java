0ab9ff3ff6e8f450c2b477f9f03fefd4c2593f01
louly <louly@citydo.com.cn>
2019-10-23 14:24:36 +0800

    "test auto deploy"

--- src/main/java/com/lly/hello/web/HelloController.java
+++ src/main/java/com/lly/hello/web/HelloController.java
@@ -1,11 +1,12 @@
 package com.lly.hello.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String sayHello() {
         return "Hello World.";
     }
